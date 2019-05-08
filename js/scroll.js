84ccc71a6d7abac017afb044e838d41ce85a7d08
alexlapinskiy <olexsandr.lapinskiy@gmail.com>
2019-05-08 11:08:10 +0300

    Reformat code

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,16 @@
-    $(document).ready(function(){
+$(document).ready(function () {
 
-        //ID блока з посиланням #prime_nav
-        $("#menu").on("click","a", function (event) {
-            //відміна стандартної обробки натискання по кнопці
-            event.preventDefault();
+    //ID блока з посиланням #prime_nav
+    $("#menu").on("click", "a", function (event) {
+        //відміна стандартної обробки натискання по кнопці
+        event.preventDefault();
 
-            //забираємо ідентифікатор блока з атрибута href
-            var id  = $(this).attr('href'),
+        //забираємо ідентифікатор блока з атрибута href
+        var id = $(this).attr('href'),
 
-                //дізнаємось висоту від початку сторінки до блока на який посилається "якір"
-                top = $(id).offset().top;
-            //анымація переходу на відстань - top за 1500 мс
-            $('body,html').animate({scrollTop: top}, 900);
-        });
+            //дізнаємось висоту від початку сторінки до блока на який посилається "якір"
+            top = $(id).offset().top;
+        //анімація переходу на відстань - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 900);
     });
+});
